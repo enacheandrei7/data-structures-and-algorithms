2480590cc4aecc7caed33fa897cb0b45e46d34ff
Enache Andrei <enache.andrei.george@gmail.com>
2023-01-30 02:48:17 +0200

    Added hash tables program and made small changes to the merge_sort_array

--- Data structures - Arrays/merge_and_sort_arrays.py
+++ Data structures - Arrays/merge_and_sort_arrays.py
@@ -9,23 +9,18 @@ def merged_sorted_array(first_arr, second_arr):
     merged_arr = []
     i = 0
     j = 0
-    first_arr_item = first_arr[0]
-    second_arr_item = second_arr[0]
 
     if len(first_arr) == 0 or len(second_arr) == 0:
         return first_arr + second_arr
 
     while i < len(first_arr) and j < len(second_arr):
 
-        if first_arr_item <= second_arr_item:
-            merged_arr.append(first_arr_item)
+        if first_arr[i] <= second_arr[j]:
+            merged_arr.append(first_arr[i])
             i += 1
-            first_arr_item = first_arr[i]
-        elif second_arr_item < first_arr_item:
-            merged_arr.append(second_arr_item)
+        elif second_arr[j] < first_arr[i]:
+            merged_arr.append(second_arr[j])
             j += 1
-            second_arr_item = second_arr[j]
+    return merged_arr + first_arr[i:] + second_arr[j:]
 
-    return merged_arr
-
-print(merged_sorted_array([0, 3, 4, 31], [4, 6, 30]))
+print(merged_sorted_array([0, 3, 4, 31, 32], [4, 6, 30, 35]))
---
+++ Data structures - Hash Tables/hash_tables.py
@@ -0,0 +1,6 @@
+"""
+Implementation of hash tables in python
+"""
+
+class HashTable():
+    """Class for implementing Hash Table data structure in Python"""
